9a88fd52753c928467527828d33db7a3e18dbbc1
jo-kwsm <ytk.kwsm@gmail.com>
2022-08-12 16:05:25 +0900

    add gpu

--- beginner_tutorial.py
+++ beginner_tutorial.py
@@ -2,37 +2,42 @@ import tensorflow as tf
 
 
 def main():
-    mnist = tf.keras.datasets.mnist
+    gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+    for device in gpu_devices:
+        tf.config.experimental.set_memory_growth(device, True)
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    with tf.device("/device:GPU:0"):
+        mnist = tf.keras.datasets.mnist
 
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train, x_test = x_train / 255.0, x_test / 255.0
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    model = tf.keras.models.Sequential(
-        [
-            tf.keras.layers.Flatten(input_shape=(28, 28)),
-            tf.keras.layers.Dense(128, activation="relu"),
-            tf.keras.layers.Dropout(0.2),
-            tf.keras.layers.Dense(10),
-        ]
-    )
+        model = tf.keras.models.Sequential(
+            [
+                tf.keras.layers.Flatten(input_shape=(28, 28)),
+                tf.keras.layers.Dense(128, activation="relu"),
+                tf.keras.layers.Dropout(0.2),
+                tf.keras.layers.Dense(10),
+            ]
+        )
 
-    predictions = model(x_train[:1]).numpy()
-    print(predictions)
-    print(tf.nn.softmax(predictions).numpy())
+        predictions = model(x_train[:1]).numpy()
+        print(predictions)
+        print(tf.nn.softmax(predictions).numpy())
 
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-    print(loss_fn(y_train[:1], predictions).numpy())
+        print(loss_fn(y_train[:1], predictions).numpy())
 
-    model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
+        model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
 
-    model.fit(x_train, y_train, epochs=5)
+        model.fit(x_train, y_train, epochs=5)
 
-    model.evaluate(x_train, y_train, verbose=2)
-    model.evaluate(x_test, y_test, verbose=2)
+        model.evaluate(x_train, y_train, verbose=2)
+        model.evaluate(x_test, y_test, verbose=2)
 
-    probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
-    print(probability_model(x_test[:5]))
+        probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+        print(probability_model(x_test[:5]))
 
 
 if __name__ == "__main__":
